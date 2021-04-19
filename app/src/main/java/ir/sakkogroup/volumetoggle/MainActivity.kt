72e60606c987eeffb3e438b02a1ea248e3e24bff
Masoud Moghaddam <masoud.mghd@gmail.com>
2021-04-19 20:43:24 +0430

    Init Project

---
+++ app/src/main/java/ir/sakkogroup/volumetoggle/MainActivity.kt
@@ -0,0 +1,13 @@
+package ir.sakkogroup.volumetoggle
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/ir/sakkogroup/volumetoggle/VolumeUpTileService.kt
@@ -0,0 +1,42 @@
+package ir.sakkogroup.volumetoggle
+
+import android.media.AudioManager
+import android.service.quicksettings.TileService
+import android.widget.Toast
+
+
+class VolumeUpTileService : TileService() {
+    override fun onClick() {
+        super.onClick()
+
+        // Called when the user click the tile
+        //val context = applicationContext
+        //Toast.makeText(context, "Volume Up", Toast.LENGTH_SHORT).show()
+        val audioManager: AudioManager = getSystemService(AUDIO_SERVICE) as AudioManager
+        audioManager.adjustVolume(AudioManager.ADJUST_RAISE,AudioManager.FLAG_SHOW_UI)
+    }
+
+    override fun onTileRemoved() {
+        super.onTileRemoved()
+
+        // Do something when the user removes the Tile
+    }
+
+    override fun onTileAdded() {
+        super.onTileAdded()
+
+        // Do something when the user add the Tile
+    }
+
+    override fun onStartListening() {
+        super.onStartListening()
+
+        // Called when the Tile becomes visible
+    }
+
+    override fun onStopListening() {
+        super.onStopListening()
+
+        // Called when the tile is no longer visible
+    }
+}
